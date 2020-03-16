7df112a921e7aea8c7c2542674b3bd96045aa087
Javier Jordan Davila Terrones <jdavilat@everis.com>
2020-03-15 23:32:07 -0500

    proteger nuestra cadena de conexion a mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ process.env.PORT = process.env.PORT || 3000;
 let urlDataBase;
 
 if (process.env.NODE_ENV) {
-    urlDataBase = 'mongodb+srv://jjordandt:u8iPKIk233BgfQfF@cluster0-47a4i.mongodb.net/coffee';
+    urlDataBase = process.env.MONGO_URI;
 } else {
     urlDataBase = 'mongodb://localhost:27017/coffee';
 }
